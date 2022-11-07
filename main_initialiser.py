abc394d87d82391156c4fb90097da5e1f6bde721
gavconn <gavin.connolly@ucdconnect.ie>
2022-11-07 16:25:31 +0000

    Black Scholes Model
    Added black scholes model.

--- main_initialiser.py
+++ main_initialiser.py
@@ -1,3 +1,10 @@
 ## class to initiate all our final results
 
 ### Need to think of a design pattern
+from bsm import BSM
+
+if __name__ == '__main__':
+    days_to_expiry = int(input("Days to expiry: "))
+    strike_price = int(input("Enter the strike price of the option: "))
+    option_type = int(("If call option enter 0, if put option enter 1"))
+    bsm = BSM(days_to_expiry, strike_price)
--- utilities.py
+++ utilities.py
@@ -1,11 +1,24 @@
 ## Class for using any utility function, mostly contains static method
 
 import os
+from jproperties import Properties
 
 class Utilities:
     
+    ## A static method to get the file path for the csv
     @staticmethod
-    def getCurrDir():
-        return os.getcwd()
+    def getFilePath(sub_path):
+        configs = Utilities.read_properties()
+        file_path = os.getcwd() + configs.get(sub_path).data
+        return file_path
 
+    #### A static method to read the environment properties file ####
+    @staticmethod
+    def read_properties():
+        configs = Properties()
+
+        with open('env.properties', 'rb') as read_prop:
+            configs.load(read_prop)
+
+        return configs
 
