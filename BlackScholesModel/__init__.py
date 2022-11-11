eac594f77e30765f9ff27b77951390d08d87995e
algoSiliguri <107167244+algoSiliguri@users.noreply.github.com>
2022-11-11 02:15:05 +0000

    Code modularity and APP

--- BlackScholesModel/__init__.py
+++ BlackScholesModel/__init__.py
@@ -4,6 +4,7 @@ import numpy as np
 import datetime as dt
 import Utilities as ut
 
+
 class BSM:
 
     def __init__(self, days_to_expiry, strike_price, call_or_put):
@@ -16,6 +17,50 @@ class BSM:
         self.interest_rates = 0
         self.dividend = 0
         self.iv = 0
+        self.mid_bid_ask = 0
+
+    ## Determine which option formula to use
+    def __option_type(self):
+
+        if self.call_or_put == 0:
+            return 1
+        else:
+            return -1
+
+    ## A function to return d1 in BSM
+    def __get_d1(self, vol=0):
+
+        if vol == 0:
+            self.calc_implied_vol()
+        else:
+            self.iv = vol
+
+        d1 = (np.log(self.spot_price/self.strike_price) + (self.interest_rates - self.dividend +
+              0.5 * self.iv**2) * self.years_to_expiry) / (self.iv * np.sqrt(self.years_to_expiry))
+        return d1
+
+    ## A function to return d2 in BSM
+    def __get_d2(self):
+
+        d1 = self.__get_d1()
+        d2 = d1 - self.iv * np.sqrt(self.years_to_expiry)
+        return d2
+
+    ## Find the option mteric data for the given date; drop all -99.99 IV data
+    def __get_om_data(self):
+
+        df_od = ut.Utilities.get_option_metric_data()
+        df_iv = df_od.loc[(df_od['Strike'] == self.strike_price) & (
+            df_od['Put=1 Call=0'] == self.call_or_put)]
+        df_iv = df_iv.loc[df_iv["Open Interest"]
+                          == df_iv["Open Interest"].max()]
+        return df_iv
+
+    ## A function to calcultae vega
+    def __calc_vega(self, vol):
+
+        d1 = self.__get_d1(vol)
+        return self.spot_price * ut.Utilities.n(d1) * np.sqrt(self.years_to_expiry)
 
     ## Calculating ineterest rates from zero curve using linear interpolation
     def calc_interest_rates(self):
@@ -43,87 +88,42 @@ class BSM:
 
         file_path = ut.Utilities.getFilePath("SPX")
         df = pd.read_csv(file_path)
-        df['Date'] = pd.TimedeltaIndex(df['Date'], unit='d') + dt.datetime(1899,12,30)
-        df_SPX = df.loc[df['Date'] == pd.Timestamp(2015,8,12)]
-        self.spot_price = df_SPX.iloc[0,5]
+        df['Date'] = pd.TimedeltaIndex(
+            df['Date'], unit='d') + dt.datetime(1899, 12, 30)
+        df_SPX = df.loc[df['Date'] == pd.Timestamp(2015, 8, 12)]
+        self.spot_price = df_SPX.iloc[0, 5]
 
     ## Calculating implied volatility of a specific strike
     def calc_implied_vol(self):
 
-        file_path = ut.Utilities.getFilePath("iv")
-        df = pd.read_csv(file_path)
-        df_od = df.loc[df['Trade dAte'] == '12/08/2015']
-        df_iv = df_od.loc[(df_od['Strike x 1000'] == self.strike_price*1000) & (df_od['Put=1 Call=0']==self.call_or_put)]
-        df_iv = df_iv.loc[df_iv["Open Interest"] == df_iv["Open Interest"].max()]
-        self.iv = df_iv.iloc[0][7]
-
-    ## Determine which option formula to use
-    def __option_type(self):
-
-        if self.call_or_put == 0:
-            return 1
-        else:
-            return -1
+        df_iv = self.__get_om_data()
+        self.mid_bid_ask = np.mean(df_iv[['Bid Price', 'Ask Price']].values)
+        self.iv = df_iv.iloc[0, 7]
 
     ## Calculating options value
-    def calc_option_value(self):
+    def calc_option_value(self, vol=0):
 
-        self.calc_dividend()
-        self.calc_interest_rates()
-        self.calc_implied_vol()
-        self.calc_spotprice_SPX()
-
-        d1 = (np.log(self.spot_price/self.strike_price) + (self.interest_rates - self.dividend + 0.5 * self.iv**2) * self.years_to_expiry) / (self.iv * np.sqrt(self.years_to_expiry))
-        d2 = d1 - self.iv * np.sqrt(self.years_to_expiry)
+        d1 = self.__get_d1(vol)
+        d2 = self.__get_d2()
         var = self.__option_type()
-        
-        #print(self.strike_price, self.spot_price, self.interest_rates, self.dividend, self.iv, self.days_to_expiry, self.call_or_put)
 
-        #print(d1, d2)
+        return var*(self.spot_price * np.exp(-self.dividend * self.years_to_expiry) * ut.Utilities.N(var*d1)
+                    - self.strike_price * np.exp(-self.interest_rates * self.years_to_expiry) * ut.Utilities.N(var*d2))
 
-        return var*(self.spot_price * np.exp(-self.dividend * self.years_to_expiry) * ut.Utilities.N(var*d1) 
-                                  - self.strike_price * np.exp(-self.interest_rates * self.years_to_expiry) * ut.Utilities.N(var*d2))
-
-    def imp_vol_option_value(self,v):
-        
-        self.calc_dividend()
-        self.calc_interest_rates()
-        self.calc_spotprice_SPX()
-        
-        #print(self.strike_price, self.spot_price, self.interest_rates, self.dividend, self.iv, self.days_to_expiry, self.call_or_put)
-        
-        d1 = (np.log(self.spot_price/self.strike_price) + (self.interest_rates - self.dividend + 0.5 * v**2) * self.years_to_expiry) / (v * np.sqrt(self.years_to_expiry))
-        d2 = d1 - v * np.sqrt(self.years_to_expiry)
-        var = self.__option_type()
-        
-        #print(d1, d2)
-
-        return var*(self.spot_price * np.exp(-self.dividend * self.years_to_expiry) * ut.Utilities.N(var*d1) 
-                                  - self.strike_price * np.exp(-self.interest_rates * self.years_to_expiry) * ut.Utilities.N(var*d2))
-    
-    def calc_vega(self,v):
-        
-        d1 = (np.log(self.spot_price/self.strike_price) + (self.interest_rates - self.dividend + 0.5 * v**2) * self.years_to_expiry) / (v * np.sqrt(self.years_to_expiry))
-        return self.spot_price * ut.Utilities.n(d1) * np.sqrt(self.years_to_expiry)
-    
+    ##  A function to find IV using Netown Raphsen algorithm
     def imp_vol_solver(self):
-        
+
         # solve for implied volatility using Newton-Raphson algorithm
-        file_path = ut.Utilities.getFilePath("iv")
-        df = pd.read_csv(file_path)
-        df_od = df.loc[df['Trade dAte'] == '12/08/2015']
-        df_iv = df_od.loc[(df_od['Strike x 1000'] == self.strike_price*1000) & (df_od['Put=1 Call=0']==self.call_or_put)]
-        
+        df_iv = self.__get_om_data()
         niter = 1000
         tol = 1.0e-5
-        v = 0.5 # initial volatility guess
-        self.mid_bid_ask = np.mean(df_iv[['Bid Price', 'Ask Price']].loc[df_iv["Volume"]==df_iv["Volume"].max()].values)
+        v = 0.5  # initial volatility guess
+        self.mid_bid_ask = np.mean(df_iv[['Bid Price', 'Ask Price']].values)
         for i in range(0, niter):
-            price = self.imp_vol_option_value(v)
-            vega = self.calc_vega(v)
+            price = self.calc_option_value(v)
+            vega = self.__calc_vega(v)
             diff = self.mid_bid_ask - price
             if (abs(diff) < tol):
-                #print(i) # Check on how fast function converges
                 return v
             v = v + diff/vega
-        return v # exit function if hasn't converged after max iterations
+        return v  # exit function if hasn't converged after max iterations
--- final_app.py
+++ final_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import BlackScholesModel as bsm
+import BlackScholesModel as bs
 import Utilities as ut
 
 ###sidebar
@@ -11,39 +11,56 @@ st.sidebar.info('Trade date : 12/08/2015', icon="ℹ️")
 
 #sidebar parameters
 
-days_to_expiry = st.sidebar.slider("Days to Expiry", value=31, min_value=7, max_value=100)
-strike_price = st.sidebar.number_input("Enter a strike price for your option (750-2500)", min_value=750, max_value=2500)
-if strike_price not in ut.Utilities.get_strike_prices():
-        st.sidebar.error("Strike price not in given data!!! Please renter!!!")
-        st.stop()
-option_type = st.sidebar.radio("Select your option type - Call-> 0 | Put-> 1",(0,1))
+days_to_expiry = st.sidebar.slider(
+    "Days to Expiry", value=31, min_value=7, max_value=100)
+
+option_type = st.sidebar.radio(
+    "Select your option type - Call-> 0 | Put-> 1", (0, 1))
+
+strike_price = st.sidebar.number_input(
+    "Enter a strike price for your option (750-2500)", min_value=750, max_value=2500,value= 2080, step=10)
+
+if strike_price not in ut.Utilities.get_strike_prices(option_type):
+    st.sidebar.error("Strike price not in given data!!! Please renter!!!")
+    st.stop()
+
+check_NRIV = st.sidebar.checkbox("Newton Raphson Volatility Solver")
 
 
-bsm = bsm.BSM(days_to_expiry, strike_price, option_type)
+bsm = bs.BSM(days_to_expiry, strike_price, option_type)
 
-###main_section
+### Main_section ###
 #title
 
 st.title("Options Pricing model")
 st.subheader("Derivative Securities Project - Group 15")
 
-col1, col2, col3, col4 = st.columns(4)  
+col1, col2, col3, col4, col5 = st.columns(5)
 
 with col1:
-    #BSM.calc_interest_rates(days_to_expiry)
     bsm.calc_interest_rates()
-    st.metric("Interest rate (%)",round(bsm.interest_rates*100,5))
+    st.metric("Interest rate (%)", round(bsm.interest_rates*100, 3))
 
 with col2:
-    st.metric("Dividend Yield (%)",1.903)
+    bsm.calc_dividend()
+    st.metric("Dividend Yield (%)", round(bsm.dividend * 100, 3))
 
 with col3:
-    #BSM.calc_option_value()
-    st.metric("Option Price ($)",round(bsm.calc_option_value(),5))
+    bsm.calc_spotprice_SPX()
+    st.metric("SPX Spot Price", round(bsm.spot_price,2))
 
 with col4:
-    st.metric("Bid-ask spread",10)
-    
-# bsm = bsm.BSM(30, 2090, 0)
-# print(bsm.calc_option_value())
-# print(bsm.imp_vol_solver())
+    st.metric("BSM Option Price($)", round(bsm.calc_option_value(), 3))
+
+with col5:
+    st.metric("Given Option IV", round(bsm.iv,3))
+
+col1, col2, col3, col4, col5 = st.columns(5)
+with col4:
+    if check_NRIV:
+        st.metric("Given Option Price($)", round(bsm.mid_bid_ask,3))
+
+with col5:
+    if check_NRIV:
+        bsm.imp_vol_solver()
+        st.metric("Calculated Option IV", round(bsm.iv,3))
