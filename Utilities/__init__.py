365b9ebfe017323c51b539018a18db7939105e91
gav-conn <gavinconnolly1@gmail.com>
2022-11-30 15:50:17 +0000

    Resolving merge conflicts#1

--- Utilities/__init__.py
+++ Utilities/__init__.py
@@ -82,9 +82,9 @@ class Utilities:
             st.line_chart(df_plot, x="Implied Volatility (Percent)",
                           y="Black Scholes Option Price")
 
-        if 'Taylor-Series Approximation' in df_plot:
+        if '1st-Order Taylor-Series Approximation' in df_plot:
             st.line_chart(df_plot, x="Spot Price", y={
-                          "Black Scholes Option Price", "Taylor-Series Approximation"})
+                "Black Scholes Option Price", "1st-Order Taylor-Series Approximation", "2nd-Order Taylor-Series Approximation"})
 
-        if pd.Timestamp(2015,8,12) in df_plot:
-            st.line_chart(df_plot)               
+        if pd.Timestamp(2015, 8, 12) in df_plot:
+            st.line_chart(df_plot)
